d4546d3a0a4b500c4302811f7bd1f7689f9d6d01
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-10-27 01:05:07 +0300

    declared variables to interact with theelements such as search-button, search-input,list of books and completed books

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded",function() {
+    const searchButton = document.getElementById("search-button");
+    const searchInput = document.getElementById("search-input");
+    const ListOfBooks = document.getElementById("list-of-books");
+    const CompletedBooks = document.getElementById("completed-books");
+
+
+    
+     
+     
+
+
+})
