345b79aa6e2ef1b62e5af596600260351bbe31ad
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-10-27 01:33:54 +0300

    Added a function that fetches data from gooogle books api

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ document.addEventListener("DOMContentLoaded",function() {
     const ListOfBooks = document.getElementById("list-of-books");
     const CompletedBooks = document.getElementById("completed-books");
 
+    const googleBooksApiKey = 'AIzaSyDy4vugqUq0ePBPWbC2SViNJS_JlTdoN14';
+
+    // Function that fetches data from Google Books Api
+    function searchBooks() {
+        const searchTerm = searchInput.value;
+    
+        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${googleBooksApiKey}`;
 
     
      
